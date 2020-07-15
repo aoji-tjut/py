63afe958bbfa3e47de1121bce89332b6feb08984
aoji-tjut <55875798+aoji-tjut@users.noreply.github.com>
2020-07-15 22:01:12 +0800

    Add files via upload

---
+++ grammar/day02/code/print.py
@@ -0,0 +1,14 @@
+# print.py
+
+print(1, 2, 3, 4)
+print()
+print(1, 2, 3, 4, 5, 6, 7, 8 + 100)
+print(1, 2, 3, 4, sep='     ')
+print(1, 2, 3, 4, sep='#')
+print(1, 2, 3, 4, sep='<->')
+print(1, 2, 3, 4, sep='')
+print(1, 2, 3, 4, end='\n')
+print(1, 2, 3, 4, end='')
+print(5, 6, 7, 8, end='')
+print(9, 10, 11, 12, end='####\n"haha"\n"哈哈"\n')
+
